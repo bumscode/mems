2c45fda9bc58d7723950dbfc1e1904e101284fbc
wellá <info@lost.design>
2021-10-15 17:21:01 +0200

    ♻️ change meme config

--- config/meme.php
+++ config/meme.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
-    'whitelisted_domains' => env('WHITELISTED_DOMAINS', 'domain.com'),
+    'allowed_domains' => env('WHITELISTED_DOMAINS') . ',' . env('PENDING_DOMAINS'),
+    'whitelisted_domains' => env('WHITELISTED_DOMAINS', 'domain.com,google.com'),
+    'pending_domain' => env('PENDING_DOMAINS', 'other-domain.com,bar.com'),
+
     'admin_email' => env('ADMIN_EMAIL', 'admin@domain.com')
 ];
